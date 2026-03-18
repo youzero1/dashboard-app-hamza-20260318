22513525c9369ef340897fb0df5692104bd13519
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-18 13:59:39 +0500

    Add src/lib/entities/User.ts

---
+++ src/lib/entities/User.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column({ default: 'user' })
+  role!: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
